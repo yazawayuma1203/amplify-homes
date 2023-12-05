38daf1857647ab9438477c06c77df30d2a8162b5
Yuma Yazawa <yazawa19980202@gmail.com>
2023-12-05 16:35:39 +0900

    Figmaでコンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import {HomeCreateForm} from './ui-components';
+import { HomeCreateForm } from './ui-components';
+import { ActionCard } from './ui-components';
 
 function App() {
   return (
     <div className="App">
     <HomeCreateForm />
+    <ActionCard />
     </div>
   );
 }
